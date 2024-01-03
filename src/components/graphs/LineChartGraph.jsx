d3bf37d80df81fbaa648061f62eb86a86e00b5f2
Amarnadh <cpamarnadh843@gmail.com>
2024-01-03 15:25:43 +0530

    feat(Design-Dashboard):Designed the garphs and Dashboard component partially

---
+++ src/components/graphs/LineChartGraph.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { LineChart } from "@mui/x-charts/LineChart";
+
+function LineChartGraph() {
+  return (
+    <div>
+      <LineChart
+        xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
+        series={[
+          {
+            data: [2, 5.5, 2, 8.5, 1.5, 5],
+          },
+        ]}
+        width={500}
+        height={300}
+      />
+    </div>
+  );
+}
+
+export default LineChartGraph;
