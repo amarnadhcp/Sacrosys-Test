ab9121ad0b9d622972dd2b945b7328fd8e20e984
Amarnadh <cpamarnadh843@gmail.com>
2024-01-03 15:39:29 +0530

    feat(dashboard-Design updation):updated the designing more responsive

--- src/components/graphs/LeastProduct.jsx
+++ src/components/graphs/LeastProduct.jsx
@@ -3,87 +3,63 @@ import React from 'react'
 function LeastProduct() {
   return (
     <>
-    <div className="w-full flex mt-2 items-center mb-2">
-      <h1 className="w-20">Open item</h1>
-      <span
-        style={{ width: "55%" }}
-        className=" bg-blue-200 rounded-r-lg "
-      >
-        <div
-          style={{ width: "100%" }}
-          className="h-full bg-violet-500 rounded-r-lg  flex justify-center"
-        >
-          <span className="text-white text-xs mx-2">99 AED</span>
-        </div>
-      </span>
-      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+      <div className="w-full flex mt-2 items-center mb-2">
+        <h1 className="w-20">Open item</h1>
+        <span style={{ width: "55%" }} className=" bg-blue-200 rounded-r-lg ">
+          <div
+            style={{ width: "100%",height: "8px"  }}
+            className="h-full bg-violet-500 rounded-r-lg  flex justify-center"
+          >
+          </div>
+        </span>
+        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+      </div>
+      <div className="w-full flex  items-center mb-2">
+        <h1 className="w-20">Delivery</h1>
+        <span style={{ width: "40%" }} className=" bg-blue-200  rounded-r-lg">
+          <div
+            style={{ width: "100%",height: "8px" }}
+            className="h-full bg-indigo-500 rounded-r-lg flex justify-center"
+          >
+          </div>
+        </span>
+        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+      </div>
+      <div className="w-full flex  items-center mb-2">
+        <h1 className="w-20">Sprite</h1>
+        <span style={{ width: "30%" }} className=" bg-blue-200 rounded-r-lg ">
+          <div
+            style={{ width: "100%",height: "8px" }}
+            className="h-full bg-rose-400 rounded-r-lg flex justify-center"
+          >
+          </div>
+        </span>
+        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+      </div>
+      <div className="w-full flex  items-center mb-2">
+        <h1 className="w-20">Budget m..</h1>
+        <span style={{ width: "20%" }} className=" bg-blue-200 rounded-r-lg ">
+          <div
+            style={{ width: "100%",height: "8px" }}
+            className="h-full bg-blue-500 rounded-r-lg flex justify-center"
+          >
+          </div>
+        </span>
+        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+      </div>
+      <div className="w-full flex  items-center mb-2">
+        <h1 className="w-20">Goto ser..</h1>
+        <span style={{ width: "20%" }} className=" bg-blue-200  rounded-r-lg">
+          <div
+            style={{ width: "100%" ,height: "8px"}}
+            className="h-full bg-blue-500 rounded-r-lg flex justify-center"
+          >
+          </div>
+        </span>
+        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+      </div>
+    </>
 
-    </div>
-    <div className="w-full flex  items-center mb-2">
-      <h1 className="w-20">Delivery</h1>
-      <span
-        style={{ width: "40%" }}
-        className=" bg-blue-200  rounded-r-lg"
-      >
-        <div
-          style={{ width: "100%" }}
-          className="h-full bg-indigo-500 rounded-r-lg flex justify-center"
-        >
-          <span className="text-white text-xs mx-2">86 AED</span>
-        </div>
-      </span>
-      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
-
-    </div>
-    <div className="w-full flex  items-center mb-2">
-      <h1 className="w-20">Sprite</h1>
-      <span
-        style={{ width: "30%" }}
-        className=" bg-blue-200 rounded-r-lg "
-      >
-        <div
-          style={{ width: "100%" }}
-          className="h-full bg-rose-400 rounded-r-lg flex justify-center"
-        >
-          <span className="text-white text-xs mx-2">68 AED</span>
-        </div>
-      </span>
-      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
-
-    </div>
-    <div className="w-full flex  items-center mb-2">
-      <h1 className="w-20">Budget m..</h1>
-      <span
-        style={{ width: "20%" }}
-        className=" bg-blue-200 rounded-r-lg "
-      >
-        <div
-          style={{ width: "100%" }}
-          className="h-full bg-blue-500 rounded-r-lg flex justify-center"
-        >
-          <span className="text-white text-xs mx-2">55 AED</span>
-        </div>
-      </span>
-      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
-
-    </div>
-    <div className="w-full flex  items-center mb-2">
-      <h1 className="w-20">Goto ser..</h1>
-      <span
-        style={{ width: "20%" }}
-        className=" bg-blue-200  rounded-r-lg"
-      >
-        <div
-          style={{ width: "100%" }}
-          className="h-full bg-blue-500 rounded-r-lg flex justify-center"
-        >
-          <span className="text-white text-xs mx-2">51 AED</span>
-        </div>
-      </span>
-      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
-
-    </div>
-  </>
 
   )
 }
--- src/components/graphs/TopProduct.jsx
+++ src/components/graphs/TopProduct.jsx
@@ -3,86 +3,62 @@ import React from 'react'
 function TopProduct() {
   return (
     <>
-      <div className="w-full flex mt-2 items-center mb-2">
-        <h1 className="w-20 text-sm">Chicken si..</h1>
-        <span
-          style={{ width: "55%" }}
-          className=" bg-blue-200 rounded-r-lg "
-        >
-          <div
-            style={{ width: "100%" }}
-            className="h-full bg-blue-500 rounded-r-lg  flex justify-center"
-          >
-            <span className="text-white text-xs mx-2">99 AED</span>
-          </div>
-        </span>
-        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
-      </div>
-      <div className="w-full flex  items-center mb-2">
-        <h1 className="w-20">Special b..</h1>
-        <span
-          style={{ width: "40%" }}
-          className=" bg-blue-200  rounded-r-lg"
-        >
-          <div
-            style={{ width: "100%" }}
-            className="h-full bg-indigo-500 rounded-r-lg flex justify-center"
-          >
-            <span className="text-white text-xs mx-2">86 AED</span>
-          </div>
-        </span>
-        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+    <div className="w-full flex mt-2 items-center mb-2">
+      <h1 className="w-20 text-sm">Chicken si..</h1>
+      <span style={{ width: "55%" }} className="bg-blue-200 rounded-r-lg">
+        <div
+          style={{ width: "100%", height: "8px" }}
+          className="h-full bg-blue-500 rounded-r-lg  flex justify-center"
+        ></div>
+      </span>
+      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+    </div>
 
-      </div>
-      <div className="w-full flex  items-center mb-2">
-        <h1 className="w-20">beef bico.</h1>
-        <span
-          style={{ width: "30%" }}
-          className=" bg-blue-200 rounded-r-lg "
-        >
-          <div
-            style={{ width: "100%" }}
-            className="h-full bg-rose-400 rounded-r-lg flex justify-center"
-          >
-            <span className="text-white text-xs mx-2">68 AED</span>
-          </div>
-        </span>
-        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+    <div className="w-full flex  items-center mb-2">
+      <h1 className="w-20">Special b..</h1>
+      <span style={{ width: "40%" }} className=" bg-blue-200  rounded-r-lg">
+        <div
+          style={{ width: "100%", height: "8px" }}
+          className="h-full bg-indigo-500 rounded-r-lg flex justify-center"
+        ></div>
+      </span>
+      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+    </div>
+    <div className="w-full flex  items-center mb-2">
+      <h1 className="w-20">beef bico.</h1>
+      <span style={{ width: "30%" }} className=" bg-blue-200 rounded-r-lg ">
+        <div
+          style={{ width: "100%", height: "8px" }}
+          className="h-full bg-rose-400 rounded-r-lg flex justify-center"
+        ></div>
+      </span>
+      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+    </div>
+    <div className="w-full flex  items-center mb-2">
+      <h1 className="w-20">beef ka..</h1>
+      <span style={{ width: "20%" }} className=" bg-blue-200 rounded-r-lg ">
+        <div
+          style={{ width: "100%", height: "8px" }}
+          className="h-full bg-blue-500 rounded-r-lg flex justify-center"
+        ></div>
+      </span>
+      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+    </div>
+    <div className="w-full flex  items-center mb-2">
+      <h1 className="w-20">beef br..</h1>
+      <span
+        style={{ width: "20%", height: "8px" }}
+        className=" bg-blue-200  rounded-r-lg"
+      >
+        <div
+          style={{ width: "100%" }}
+          className="h-full bg-blue-500 rounded-r-lg flex justify-center"
+        ></div>
+      </span>
+      <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
+    </div>
+  </>
 
-      </div>
-      <div className="w-full flex  items-center mb-2">
-        <h1 className="w-20">beef ka..</h1>
-        <span
-          style={{ width: "20%" }}
-          className=" bg-blue-200 rounded-r-lg "
-        >
-          <div
-            style={{ width: "100%" }}
-            className="h-full bg-blue-500 rounded-r-lg flex justify-center"
-          >
-            <span className="text-white text-xs mx-2">55 AED</span>
-          </div>
-        </span>
-        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
-
-      </div>
-      <div className="w-full flex  items-center mb-2">
-        <h1 className="w-20">beef br..</h1>
-        <span
-          style={{ width: "20%" }}
-          className=" bg-blue-200  rounded-r-lg"
-        >
-          <div
-            style={{ width: "100%" }}
-            className="h-full bg-blue-500 rounded-r-lg flex justify-center"
-          >
-            <span className="text-white text-xs mx-2">51 AED</span>
-          </div>
-        </span>
-        <span className="ml-2 text-xs flex text-center">55.5 AED/5 pc</span>
-
-      </div>
-    </>
 
   )
 }
